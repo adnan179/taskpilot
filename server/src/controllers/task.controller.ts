4b147d011abfbe33e9e95348a610fc77aa79bb33
Adnan <adnan.shaik@technoidentity.com>
2025-05-28 16:46:12 +0530

    finished- task schema, model, controller and routes

---
+++ server/src/controllers/task.controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import { TaskSchema } from "../schemas/task.schema";
+import { TaskModel } from "../models/task.model";
+import { error } from "console";
+
+export const createTask = async(req:Request, res:Response) => {
+    const parsed = TaskSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.status(400).json({error:parsed.error.flatten()});
+    }
+
+    try{
+        const task = new TaskModel(parsed.data);
+        await task.save();
+        res.status(200).json(task)
+    }catch(err:any){
+        res.status(500).json({error:`Failed to create task:${err.message}`})
+    }
+};
+
+
+export const getTasks = async(_req:Request, res:Response) => {
+    try{
+        const tasks = await TaskModel.find().sort({ createdAt:-1});
+        if(!tasks) return res.status(404).json({error:"no tasks found!"})
+        res.status(200).json(tasks);
+    }catch(err:any){
+        res.status(500).json({error:`Failed to fetch tasks:${err.message}`})
+    }
+};
+
+export const getTaskById = async(req:Request,res:Response) => {
+    try{
+        const task = await TaskModel.findById(req.params.id);
+        if(!task) return res.status(404).json({error:"No task found!"});
+
+        res.status(200).json(task)
+    }catch(err:any){
+        res.status(500).json({error:`Failed to fetch task:${err.message}`})
+    }
+}
+
+export const updateTask = async(req:Request,res:Response) => {
+    const parsed = TaskSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.status(400).json({error:parsed.error.flatten()})
+    }
+
+    try{
+        const updated = await TaskModel.findByIdAndUpdate(req.params.id, parsed.data,{new: true});
+        if(!updated) return res.status(404).json({error:"no task found!"});
+
+        res.status(200).json(updated)
+    }catch(err:any){
+        res.status(500).json({error:`Failed to update the task:${err.message}`})
+    }
+}
+
+export const deleteTask = async(req:Request, res: Response) => {
+    try{
+        const deleted = await TaskModel.findByIdAndDelete(req.params.id);
+        if(!deleted) return res.status(404).json({ error: "Task not found!"});
+
+        res.status(200).json({message:"Task deleted successfully", deleted});
+    }catch(err:any){
+        res.status(500).json({error:`failed to delete task:${err.message}`})
+    }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import categoryRoutes from "./routes/category.route";
+import taskRoutes from "./routes/task.route";
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/categories", categoryRoutes);
+app.use("/api/tasks", taskRoutes);
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
